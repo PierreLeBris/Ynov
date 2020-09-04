dbb6d30b0205233d4216bda895c39e73af10e73c
Pierre LE BRIS <50422307+PierreLeBris@users.noreply.github.com>
2020-09-04 11:43:54 +0200

    Add files via upload

--- YNOV/linux-git-python-master/apigit.py
+++ YNOV/linux-git-python-master/apigit.py
@@ -1,8 +1,9 @@
 import requests
 import json
+import sys
 
 def query_github():
-    url = "https://api.github.com/repositories"
+    url = "https://api.github.com/search/repositories?sort=stars&order=desc"
     reponse = requests.get(url)
     data = reponse.json()
     for dict in data:
@@ -10,8 +11,11 @@ def query_github():
         print(dict["description"])
 
 def read_from_local_data():
-    path = './repositories.json'
+    path = '/home/pierre/Bureau/YNOV/linux-git-python-master/repositories.json'
     file = open(path)
     content = file.read()
     file.close()
     return json.loads(content)
+
+sys.stdout = open('test.txt','wt')
+print(query_github())
